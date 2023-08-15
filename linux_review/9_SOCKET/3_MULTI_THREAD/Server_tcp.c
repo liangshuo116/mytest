161313eb64fed5dfea277c1bfcb59b79f00e2bbe
liangshuo116 <ls775282021@163.com>
2023-08-16 00:36:51 +0800

    增加服务器中转，群发功能

--- linux_review/9_SOCKET/3_MULTI_THREAD/Server_tcp.c
+++ linux_review/9_SOCKET/3_MULTI_THREAD/Server_tcp.c
@@ -12,6 +12,9 @@
 
 // 多线程结构服务器
 
+int socket_list[1024]; 		// 连接的客户端socket数组
+pthread_mutex_t list_lock; 	// 客户端socket数组锁
+
 //#define SERVER_IP 		"192.168.221.170"
 #define SERVER_PORT 	54321
 #define shutdown 		1
@@ -23,6 +26,7 @@ typedef struct
 }Thread_arg; 	// 线程需要传入的参数
 
 // 线程工作（一对一处理客户端业务）
+// 加锁，转发功能
 void* Thread_job(void* arg)
 {
 	Thread_arg targ = *(Thread_arg*)arg;
@@ -33,9 +37,10 @@ void* Thread_job(void* arg)
 	time_t tp; 					// 时间变量
 	char time_buffer[1024]; 	// 时间字符串数据
 	char Request_msg[1024]; 	// 客户端请求数据
+	char Forward[2048]; 		// 转发数据
 	bzero(Request_msg, sizeof(Request_msg));
+	bzero(Forward, sizeof(Forward));
 
-	// 读取一次客户端请求数据，读取完毕关闭连接
 	// 当前一个线程对应一个客户端，可以阻塞读取数据
 	while ((recvlen = recv(targ.client_fd, Request_msg, sizeof(Request_msg), 0)) > 0) // 循环接收当前对应客户端数据
 	{
@@ -48,9 +53,19 @@ void* Thread_job(void* arg)
 			send(targ.client_fd, time_buffer, strlen(time_buffer), 0); 	// 发送给客户端
 			printf("Response (%s) local time\n", targ.client_ip);
 		}
-		else // 没有请求时间就打印客户端数据
+		else // 没有请求时间就将客户端数据转发给其他所有已经连接的客户端（群发）
 		{
-			printf("[%s] 对你说：%s\n", targ.client_ip, Request_msg);
+			sprintf(Forward, "[%s] 对你说：%s\n\n", targ.client_ip, Request_msg);
+			printf("%s", Forward);
+			pthread_mutex_lock(&list_lock);
+			for (int i = 0; i < 1024; i++)
+			{
+				if (socket_list[i] != -1)
+				{
+					send(socket_list[i], Forward, strlen(Forward), 0);
+				}
+			}
+			pthread_mutex_unlock(&list_lock);
 		}
 		// 每次接收完数据清零等待下次接收
 		bzero(Request_msg, sizeof(Request_msg)); 				
@@ -64,6 +79,18 @@ void* Thread_job(void* arg)
 	if (recvlen == 0) 	// 客户端退出
 	{	
 		printf("%s Thread tid 0x%x Exit, Close.\n", targ.client_ip, (unsigned int)pthread_self());
+		for (int i = 0; i < 1024; i++)
+		{
+			pthread_mutex_lock(&list_lock);
+			if (socket_list[i] == targ.client_fd)
+			{
+				socket_list[i] = -1;
+				pthread_mutex_unlock(&list_lock);
+				break;
+			}
+			pthread_mutex_unlock(&list_lock);
+		}
+		
 		close(targ.client_fd);
 		pthread_exit(NULL); 	// 客户端退出，线程退出
 	}
@@ -73,6 +100,12 @@ void* Thread_job(void* arg)
 
 int main(void)
 {
+	for (int i = 0 ; i < 1024; i++)			// 初始化客户端socket数组
+	{
+		socket_list[i] = -1;
+	}
+	pthread_mutex_init(&list_lock, NULL); 	// 初始化互斥锁
+	
 	char Response_msg[1024]; 	// 服务端回应数据
 	socklen_t Addrlen; 			// 网络信息结构体大小
 
@@ -102,6 +135,19 @@ int main(void)
 		// 连接成功，获取客户端信息	
 		bzero(targ.client_ip, sizeof(targ.client_ip));
 		inet_ntop(AF_INET, &ClientAddr.sin_addr.s_addr, targ.client_ip, sizeof(targ.client_ip));
+		
+		// 存储客户端套接字
+		pthread_mutex_lock(&list_lock);
+		for (int i = 0; i < 1024; i++)
+		{
+			if (socket_list[i] == -1)
+			{
+				socket_list[i] = targ.client_fd;
+				break;
+			}
+		}
+		pthread_mutex_unlock(&list_lock);
+		
 		// 创建普通线程
 		if ((err = pthread_create(&tid, NULL, Thread_job, (void*)&targ)) > 0)
 		{
@@ -114,10 +160,10 @@ int main(void)
 		printf("Master 0x%x Client ip %s port %d , Connection Successly.\n", (unsigned int)pthread_self(), targ.client_ip, ntohs(ClientAddr.sin_port)); 	// 显示父进程id以及客户端连接信息
 		sprintf(Response_msg, "Hi, %s , welcome to Test Server [%s] Version 0.0.1~\n", targ.client_ip, Server_ip);
 		send(targ.client_fd, Response_msg, strlen(Response_msg), 0); 	// 向客户端发送响应数据
-		printf("Send Response_msg to Client:%s\n", targ.client_ip);	
-
+		printf("Send Response_msg to Client:%s, ClientSock:%d\n", targ.client_ip, targ.client_fd);	
 	}
 	close(Server_fd); 	// 5.关闭服务器端套接字
+	pthread_mutex_destroy(&list_lock); // 回收互斥锁
 
 	return 0;
 }
