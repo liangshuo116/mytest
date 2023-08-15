38e63140a3a82ef845373983218ee4b209f01138
liangshuo116 <ls775282021@163.com>
2023-08-16 00:46:16 +0800

    服务器中转群发功能

--- linux_review/9_SOCKET/3_MULTI_THREAD/Client_tcp.c
+++ linux_review/9_SOCKET/3_MULTI_THREAD/Client_tcp.c
@@ -3,20 +3,24 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <MysocketAPI.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 //#define SERVER_IP "42.193.104.238" 
-#define SERVER_IP "192.168.221.170" 
+#define SERVER_IP "192.168.221.171" 
 //#define SERVER_PORT 8080
 #define SERVER_PORT 54321
 
 int main(void)
 {
-	char Response_msg[1024];
-	char Request[1500];
-	char server_ip[16];
+	char Response_msg[1024]; 	// 服务端回应数据
+	char Request_msg[1500]; 	// 客户端请求数据
+	char server_ip[16]; 		// 服务端IP
 	int recvlen;
 	socklen_t Addrlen;
-	
+
 	struct sockaddr_in ServerAddr;
 	bzero(&ServerAddr, sizeof(ServerAddr));
 	ServerAddr.sin_family = AF_INET;
@@ -31,23 +35,66 @@ int main(void)
 	Addrlen = sizeof(ServerAddr);
 	CONNECT(Client_fd, (struct sockaddr*)&ServerAddr, Addrlen);
 
-	while (shutdown)
+	//读取服务器端返回数据
+	bzero(Response_msg, sizeof(Response_msg));
+	recvlen = recv(Client_fd, Response_msg, sizeof(Response_msg), 0); // 阻塞接收数据
+	if (recvlen > 0)
 	{
-		//读取服务器端返回数据
-		bzero(Request, sizeof(Request));
-		recvlen = recv(Client_fd, Request, sizeof(Request), MSG_DONTWAIT); // 非阻塞接收数据
-		if (recvlen > 0)
-		{
-			printf("对你说：%s\n", Request);
-		}
-		//向服务器端发送数据
-		bzero(Response_msg, sizeof(Response_msg));
-		//sprintf(Response_msg, "localtime\n");
-		//读取标准输入，而后发送信息
-		//fgets(Response_msg, sizeof(Response_msg), stdin);
-		read(STDIN_FILENO, Response_msg, sizeof(Response_msg)); //阻塞读标准输入
-		send(Client_fd, Response_msg, strlen(Response_msg), 0);
+		printf("对你说：%s\n", Response_msg);
 	}
+	
+	bzero(Request_msg, sizeof(Request_msg));
+	// read默认为阻塞读取，现在改为非阻塞读取
+	int len;
+	int flags;
+	flags = fcntl(STDIN_FILENO, F_GETFL, 0); 	// 将 STD_IN 默认的 flags 选项获取并返回
+	flags |= FNONBLOCK; 						// 加入非阻塞标签
+	fcntl(STDIN_FILENO, F_SETFL, flags); 		// 将 STDIN_FILENO 设置为非阻塞
+again:
+	//读取标准输入，而后发送信息（非阻塞）
+	while ((len = read(STDIN_FILENO, Request_msg, sizeof(Request_msg))) > 0)
+	{
+		//向服务器端发送数据
+		send(Client_fd, Request_msg, strlen(Request_msg), 0);
+		usleep(100000); //等待服务端处置
+
+		//读取服务器端返回数据
+		bzero(Response_msg, sizeof(Response_msg));
+		recvlen = recv(Client_fd, Response_msg, sizeof(Response_msg), MSG_DONTWAIT); // 非阻塞接收数据
+		if (recvlen == -1)
+		{
+			if (errno == EAGAIN) 	// 未收到服务端数据
+			{
+				bzero(Request_msg, sizeof(Request_msg)); 	// 清空请求数据，方便下次发送
+				continue;
+			}
+			else
+			{
+				perror("Recv Call Failed!\n");
+			}
+		}
+		printf("%s", Response_msg);
+		bzero(Request_msg, sizeof(Request_msg));
+		bzero(Response_msg, sizeof(Response_msg));
+	}
+	if (len == -1)
+	{
+		if (errno == EAGAIN) // 未从标准输入读到数据
+		{
+			if (recv(Client_fd, Response_msg, sizeof(Response_msg), MSG_DONTWAIT) == -1)
+			{
+				if (errno == EAGAIN) 	// 未收到服务端发来的数据
+					bzero(Request_msg, sizeof(Request_msg));
+				else
+					perror("Recv Call Failed!\n");
+			}
+			printf("%s", Response_msg); // 收到服务端发来的数据， 打印
+			bzero(Request_msg, sizeof(Request_msg));
+			bzero(Response_msg, sizeof(Response_msg));
+		}
+	}
+	goto again; 	// 回到again处，一直循环读取标准输入
+	
 	close(Client_fd);
 	return 0;
 }
